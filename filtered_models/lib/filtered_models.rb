f0138472fdc84ec2c662dba79bb9ea8c37f97c4a
Fernando Doglio <deleteman@gmail.com>
2011-08-12 17:27:59 -0300

    Adding code

---
+++ filtered_models/lib/filtered_models.rb
@@ -0,0 +1,39 @@
+# FilteredModels
+class ::ActiveRecord::Base
+
+	private 
+	def execute_methods options, current_method
+		options.each do |event, methods|
+			if !methods.empty? then
+				methods.each do |filtered_method|
+					if filtered_method == current_method then
+						self.send event
+					end
+				end
+			else 
+				self.send event
+			end
+
+		end
+	end
+
+	public
+	def self.before_filter options = {}
+		@@before_filter_options = options
+	end
+
+	def self.after_filter options={}
+		@@after_filter_options = options
+	end
+
+
+	def method_missing(current_method, *args)
+		begin
+			super 
+		rescue
+			execute_methods @@before_filter_options, current_method
+			self.send "__#{current_method}".to_sym, *args
+			execute_methods @@after_filter_options, current_method
+		end
+	end
+end
---
+++ filtered_models/test/filtered_models_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FilteredModelsTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
