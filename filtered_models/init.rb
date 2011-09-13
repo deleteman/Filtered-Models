e87255c506d059ae0f2f8203992323e3fba68287
Fernando Doglio <deleteman@gmail.com>
2011-09-13 10:53:11 -0300

    Adding support for Rails 2.3.x, needs more testing

--- filtered_models/init.rb
+++ filtered_models/init.rb
@@ -4,5 +4,14 @@ require 'filtered_models'
 class ActiveRecord::Base
 	extend FilterModels::ClassLevelFilteredMethod
 
-	after_initialize :load_filters
+	
+	if !Rails::VERSION::STRING.start_with?("2.")
+		after_initialize :load_filters
+	else
+		def after_initialize 
+			load_filters
+		end
+
+	end
+
 end
